bf16c23e01ed3f689aae51ea8db2efeafb16db1d
alana <alanjiraya77@gmail.com>
2017-08-28 10:07:13 -0500

    Se hizo la configuracion inicial para poder empezar a trabajar

---
+++ www/page-home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>FCQ</title>
+
+<!-- STYLE ZONE -->
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="css/swiper.min.css">  
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/foundation.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/hover.css">
+	<link rel="stylesheet" href="css/swal.css">
+<!-- STYLE ZONE -->
+
+</head>
+<body>
+
+
+
+<!-- SCRIPT ZONE -->
+    <script src="<?php res(); ?>/js/swiper.jquery.min.js"></script>
+    <script src="<?php res(); ?>/js/swiper.min.js"></script>
+    <script src="<?php res(); ?>/js/jquery.min.js"></script>
+    <script src="<?php res(); ?>/js/foundation.min.js"></script>
+    <script src="<?php res(); ?>/js/skrollr.min.js"></script>
+	<script src="<?php res(); ?>/js/mandrill.js"></script>
+    <script src="<?php res(); ?>/js/swal.js"></script>
+    <script src="<?php res(); ?>/js/nicescroll.js"></script>
+    <script src="<?php res(); ?>/script.js"></script>
+
+<!-- SCRIPT ZONE -->
+
+</body>
+</html>
