abf5a614d4ab2f756aa51493223eb33413474477
alana <alanjiraya77@gmail.com>
2017-09-11 17:41:34 -0500

    mucho

--- www/script.js
+++ www/script.js
@@ -2,3 +2,14 @@ $(document).ready(function(){
 	$('html').niceScroll();
 	$(document).foundation();
 });
+document.addEventListener('init', function(event) {
+  var page = event.target;
+
+  if (page.id === 'page1') {
+    page.querySelector('#push-button').onclick = function() {
+      document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Post'}});
+    };
+  } else if (page.id === 'page2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  }
+});
