4cfa6f3b5c451dbb4dd31fd9d6932c2a0ed79318
alana <alanjiraya77@gmail.com>
2017-09-13 10:42:03 -0500

    mucho1

--- www/script.js
+++ www/script.js
@@ -1,15 +1,36 @@
 $(document).ready(function(){
 	$('html').niceScroll();
 	$(document).foundation();
+  addcard();
+  addposts();
 });
-document.addEventListener('init', function(event) {
-  var page = event.target;
+//Funcion para la navegacion entre ventanas
+function pushpage(idevento){
+    document.querySelector('#myNavigator').pushPage(idevento+'.html', {data: {title: 'Post'}});
+    //page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+}
+//Funcion para agregar cartas
+function addcard(){
 
-  if (page.id === 'page1') {
-    page.querySelector('#push-button').onclick = function() {
-      document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Post'}});
-    };
-  } else if (page.id === 'page2') {
-    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
-  }
-});
+  $(".cards").html("");
+
+  $.getJSON('http://fcq.claresti.com/api/?o=posts', function(json, textStatus) {
+      $.each(json,function(le, item) {
+        $(".cards").append('<div class="card_home"><div class="card_img"><div class="card_title">'+item["title"].substring(0,25)+'...'+'</div></div><div class="card_content"><div class="card_fecha">'+item["id"]+'</div><div class="card_compartir" id="push-button" onclick="pushpage('+item["id"]+')"><img src="img/share-option.png" alt="" class="compartir"></div><div class="card_text">'+item["excerpt"]+'</div></div></div>');
+        $(".card_img").css('background-image', 'url("'+item["image"]+'")');
+
+      });
+
+  });
+}
+function addposts(){
+
+  $(".posts").html("");
+
+  $.getJSON('http://fcq.claresti.com/api/?o=posts', function(json, textStatus) {
+      $.each(json,function(le, item) {
+        $(".posts").append('<template id="'+item["id"]+'.html"><ons-page id="page2"><ons-toolbar><div class="left"><ons-back-button>Inicio</ons-back-button></div><div class="center">Post</div></ons-toolbar><div class="img_single" style="background-image: url(' + "'" + item['image'] + "'" + ')"><div class="title_single"><strong>'+item["title"]+'</strong></div><p class="fecha_single">'+item["id"]+'</p><div class="contenido_single">'+item["content"]+'</div></div></ons-page></template>');
+      });
+
+  });
+}
