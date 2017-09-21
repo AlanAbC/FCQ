7c5642d4ad4b38e66174356f49f459d94de42154
Ricardo Pizano <ricardo.p@iw.digital>
2017-09-21 12:55:13 -0500

    cambi de funciones para llenar los post

--- www/script.js
+++ www/script.js
@@ -1,3 +1,5 @@
+var urlCat = 'http://fcq.claresti.com/api/?o=posts_cat&cat='
+
 $(document).ready(function(){
 	$('html').niceScroll();
 	$(document).foundation();
@@ -17,8 +19,8 @@ document.addEventListener('deviceready', function () {
 //Funcion para llamar las funciones que cargan la informacion de la pantalla y muestra el loader
 function cargarInicio() {
     $('.loader').show();
-    addcard();
-    addposts();
+    addcard('http://fcq.claresti.com/api/?o=posts');
+    addposts('http://fcq.claresti.com/api/?o=posts');
     $('.loader').hide();
 }
 //Funcion para desplegar el menu
@@ -31,26 +33,26 @@ function pushpage(idevento){
     //page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
 }
 //Funcion para agregar cartas
-function addcard(){
+function addcard(url){
 
   $(".cards").html("");
 
-  $.getJSON('http://fcq.claresti.com/api/?o=posts', function(json, textStatus) {
+  $.getJSON(url, function(json, textStatus) {
       $.each(json,function(le, item) {
-        $(".cards").append('<div class="card_home"><div class="card_img"><div class="card_title">'+item["title"].substring(0,25)+'...'+'</div></div><div class="card_content"><div class="card_fecha">'+item["id"]+'</div><div class="card_compartir" id="push-button" onclick="pushpage('+item["id"]+')"><img src="img/share-option.png" alt="" class="compartir"></div><div class="card_text">'+item["excerpt"]+'</div></div></div>');
+        $(".cards").append('<div class="card_home"><div class="card_img"><div class="card_title">'+item["title"].substring(0,25)+'...'+'</div></div><div class="card_content"><div class="card_fecha">'+item["date"]+'</div><div class="card_compartir" id="push-button" onclick="pushpage('+item["id"]+')"><img src="img/share-option.png" alt="" class="compartir"></div><div class="card_text">'+item["excerpt"]+'</div></div></div>');
         $(".card_img").css('background-image', 'url("'+item["image"]+'")');
 
       });
 
   });
 }
-function addposts(){
+function addposts(url){
 
   $(".posts").html("");
 
-  $.getJSON('http://fcq.claresti.com/api/?o=posts', function(json, textStatus) {
+  $.getJSON(url, function(json, textStatus) {
       $.each(json,function(le, item) {
-        $(".posts").append('<template id="'+item["id"]+'.html"><ons-page id="page2"><ons-toolbar><div class="left"><ons-back-button>Inicio</ons-back-button></div><div class="center">Post</div></ons-toolbar><div class="img_single" style="background-image: url(' + "'" + item['image'] + "'" + ')"><div class="title_single"><strong>'+item["title"]+'</strong></div><p class="fecha_single">'+item["id"]+'</p><div class="contenido_single">'+item["content"]+'</div></div></ons-page></template>');
+        $(".posts").append('<template id="'+item["id"]+'.html"><ons-page id="page2"><ons-toolbar><div class="left"><ons-back-button>Inicio</ons-back-button></div><div class="center">Post</div></ons-toolbar><div class="img_single" style="background-image: url(' + "'" + item['image'] + "'" + ')"><div class="title_single"><strong>'+item["title"]+'</strong></div><p class="fecha_single">'+item["date"]+'</p><div class="contenido_single">'+item["content"]+'</div></div></ons-page></template>');
       });
 
   });
