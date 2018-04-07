b2b8468bc76ad4cdd504729518944f0afc52a78e
zimoo354 <zimoo354@gmail.com>
2018-04-07 00:32:29 -0500

    Cambios para onesignal

--- platforms/android/assets/www/script.js
+++ platforms/android/assets/www/script.js
@@ -24,30 +24,30 @@ document.addEventListener('deviceready', function () {
   window.plugins.OneSignal
   .startInit("ee3ed7e5-a647-44f4-9e8e-e756e7147518")
   .handleNotificationOpened(notificationOpenedCallback)
+  .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
   .endInit();
+
 }, false);
 //Funcion para llamar las funciones que cargan la informacion de la pantalla y muestra el loader
 function cargarInicio() {
-  addcard(baseUrl + 'posts');
-  addposts(baseUrl + 'posts');
+  addcard(baseUrl + 'posts_fac&fac=' + localStorage.getItem('faculti'));
   loadMenu();
 }
 function cargarFacultades(){
   var container = $("#catcontainer");
   container.html('');
-  var url = baseUrl + "faculties"
+  var url = baseUrl + "faculties";
   $.getJSON(url, function(json, textStatus){
      container.html('');
     $.each(json, function(index, item) {
-      var icon = item['image'] || 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/No_icon_red.svg/1000px-No_icon_red.svg.png';
+      var icon = item['image'] || 'img/college-graduation.png';
       faculties.push(item["slug"]);
       facultiesFlags.push(false);
-      var item_category = 
+      var item_category =
       $('<div>')
       .addClass('catElement')
       .attr('style', 'background: ' + item['color'] + '!important')
-      .attr('onclick', 'beginPosts('+item["term_id"]+')')
-      .attr('id', item['term_id'])
+      .attr('onclick', 'beginPosts("'+item["slug"]+'")')
       .append(
         $('<p>')
         .addClass('catElementText')
@@ -71,14 +71,34 @@ function cargarFacultades(){
 //Funcion para desplegar el menu
 function pushmenu(){
   $('.ui.labeled.icon.sidebar').sidebar('toggle');
-
+}
+//Funcion que despliega la pagina de busueda
+function pushSearch(){
+  document.querySelector('#myNavigator').pushPage('pagebus.html', {data: {title: 'Busqueda'}});
 }
 //Funcion para la navegacion entre ventanas
 function pushpage(idevento){
-  document.querySelector('#myNavigator').pushPage(idevento+'.html', {data: {title: 'Post'}});
+  $(".img_single").attr('style', 'display: none !important');
+  document.querySelector('#myNavigator').pushPage('single.html', {data: {title: 'Post'}});
+  url = baseUrl + 'post&id=' + idevento;
+  $.getJSON(url, function(json, textStatus) {
+    $('.title_single').html(json[0].title);
+    $('.fecha_single').html(json[0].date);
+    $('.contenido_single').html(json[0].content);
+    $(".loaderSingle").attr('style', 'display: none !important');
+    $(".img_single").attr('style', 'display: block !important');
+    $('.img_single').css('background-image', 'url(' + json[0].image  + ')');
+  });
+
     //page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
   }
   function pushFaculties() {
+       var temp = localStorage.getItem('faculti');
+      var array = temp.split(',');
+      for(i = 0; i< array.length; i++){
+          window.plugins.OneSignal.deleteTag(array[i]);
+      }
+      localStorage.removeItem("faculti");
       document.querySelector('#myNavigator').pushPage('pageFacult.html');
       var myNavigator = document.querySelector('ons-navigator');
       myNavigator.addEventListener('postpush', function(event) {
@@ -94,33 +114,34 @@ function beginPosts(idFacultad){
       $("#" + idFacultad). css('background-color', 'white').parents('.catElement').addClass('active');
       facultiesFlags[index] = true;
   }
-  
-  localStorage.setItem('faculti' ,idFacultad);
+
+  /*localStorage.setItem('faculti' ,idFacultad);
   document.querySelector('#myNavigator').replacePage('page1.html', {data: {title: 'Inicio'}});
-  cargarInicio();
+  cargarInicio();*/
 }
   function home_card_template(le, item) {
-    return '<div class="card_home"><div class="card_img" id="item-' + le + '"><div class="card_title">'+item["title"].substring(0,25)+'...'+'</div></div><div class="card_content"><div class="card_fecha">'+item["date"]+'</div><div class="card_compartir" id="push-button" onclick="pushpage('+item["id"]+')"><img src="img/share-option.png" alt="" class="compartir"></div><div class="card_text">'+item["excerpt"]+'</div></div></div>';
+    return '<div class="card_home" onclick="pushpage('+item["id"]+')"><div class="card_img" id="item-' + le + '"><div class="card_title">'+item["title"].substring(0,25)+'...'+'</div></div><div class="card_content"><div class="card_fecha">'+item["date"]+'</div><div class="card_compartir" id="push-button"><img src="img/share-option.png" alt="" class="compartir"></div><div class="card_text">'+item["excerpt"]+'</div></div></div>';
   }
 
 //Funcion para agregar cartas
 function addcard(url){
-  loader.attr('style', 'display: block !important');
   $(".cards").html("");
-
+  $(".loaderCards").attr('style', 'display: block !important');
   $.getJSON(url, function(json, textStatus) {
     $.each(json,function(le, item) {
       $(".cards").append(home_card_template(le, item));
       $(".card_img#item-" + le).css('background-image', 'url("'+item["image"]+'")');
-
+      $(".loaderCards").attr('style', 'display: none !important');
     });
 
   });
   loader.attr('style', 'display: none !important');
 }
 function savefaculties(){
-     for (i = 0; i < facultiesFlags.length; i++) { 
+     for (i = 0; i < facultiesFlags.length; i++) {
+      debugger;
         if(facultiesFlags[i]){
+            window.plugins.OneSignal.sendTag(faculties[i], faculties[i]);
             sendFaculties.push(faculties[i])
         }
     }
@@ -132,17 +153,14 @@ function savefaculties(){
         cargarInicio();
     }
 }
-function addposts(url){
-
+/*function addposts(url){
   $(".posts").html("");
-
   $.getJSON(url, function(json, textStatus) {
     $.each(json,function(le, item) {
       $(".posts").append('<template id="'+item["id"]+'.html"><ons-page id="page2"><ons-toolbar><div class="left"><ons-back-button>Inicio</ons-back-button></div><div class="center">Post</div></ons-toolbar><div class="img_single" style="background-image: url(' + "'" + item['image'] + "'" + ')"><div class="title_single">'+item["title"]+'</div><p class="fecha_single">'+item["date"]+'</p><div class="contenido_single">'+item["content"]+'</div></div></ons-page></template>');
     });
-
   });
-}
+}*/
 function filterposts(cat){
   url = baseUrl + "posts_cat&cat="+cat;
   addcard(url);
@@ -150,9 +168,7 @@ function filterposts(cat){
 
 }
 function homeposts(){
-  url = baseUrl + "posts"
-  addcard(url);
-  addposts(url);
+  addcard(baseUrl + 'posts_fac&fac=' + localStorage.getItem('faculti'));
 }
 function searchposts(){
   loader.attr('style', 'display: block !important');
@@ -182,12 +198,12 @@ function searchposts(){
 function loadMenu(){
   var menu = $("#menu");
   menu.html("");
-  menu.append('<a class="item" href="javascript:void(0)" onclick="pushmenu();homeposts();"><i class="ion-home icon"></i>Home </a> <a class="item" onclick="pushmenu();pushpage(\'pagebus\');"> <i class="ion-search icon"></i>Busqueda </a><a class="item" onclick="pushmenu();pushpage(\'pageFacult\');setTimeout(cargarFacultades(), 1000);"> <i class="ion-ios-bookmarks icon"></i>Facultades </a>');
+  menu.append('<a class="item" href="javascript:void(0)" onclick="pushmenu();homeposts();"><i class="ion-home icon"></i>Home </a> <a class="item" onclick="pushmenu();pushSearch();"> <i class="ion-search icon"></i>Busqueda </a><a class="item" id="facultades" onclick="pushmenu();pushFaculties();"> <i class="ion-ios-bookmarks icon"></i>Facultades </a>');
   var url = baseUrl + "categories"
   $.getJSON(url, function(json, textStatus){
     $.each(json, function(index, item) {
-      var icon = item['icon'] || 'ion-alert-circled';
-      var item_menu = 
+      var icon = item['icon'] || 'ion-android-happy';
+      var item_menu =
       $('<a>')
       .attr('id', item['slug'])
       .attr('href', 'javascript:void(0)')
